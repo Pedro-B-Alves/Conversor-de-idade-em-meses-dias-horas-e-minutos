92d5cb9372bf055ebdf9fb935d7c0f9261a983d7
Pedro Alves <pedro22carro@gmail.com>
2020-11-13 18:02:34 -0300

    Calculo finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exe._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+            int idade=int.Parse(Console.ReadLine());
+
+            int meses=idade*12;
+            int dias=idade*365;
+            int horas=dias*24;
+            int minutos=horas*60;
+
+            Console.WriteLine("Idade em meses "+meses);
+            Console.WriteLine("Idade em dias "+dias);
+            Console.WriteLine("Idade em horas "+horas);
+            Console.WriteLine("Idade em minutos "+minutos);
+        }
+    }
+}
